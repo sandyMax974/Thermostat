7fa1b5233428c30ea940e64d09b51190ecf169ce
sandy <pounoussamy.sandy@gmail.com>
2021-03-10 18:19:34 +0000

    implemented weather API

--- interface.js
+++ interface.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 let thermostat = new Thermostat()
 
 updateTemperature();
@@ -32,5 +33,22 @@ $("#powersaving-on").click(function() {
 
 function updateTemperature() {
   $('#temperature').text(thermostat.getTemperature());
+}
+
+displayWeather('London')
+
+$("#current-city").change(function() {
+  var city = $("#current-city").val();
+  displayWeather(city);
+})
+
+function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed'; 
+  var units= '&units=metric'
+
+  $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
 }
 });
