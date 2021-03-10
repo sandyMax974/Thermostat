1c761ed6dc4d2db985ae29423215a7d181524510
sandy <pounoussamy.sandy@gmail.com>
2021-03-10 17:11:04 +0000

    added all jQuery

--- interface.js
+++ interface.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  let thermostat = new Thermostat()
+let thermostat = new Thermostat()
+
 $('#temperature').text(thermostat.getTemperature());
 
 $("#temperature-up").click(function() { 
@@ -10,6 +11,23 @@ $("#temperature-up").click(function() {
 $("#temperature-down").click(function() { 
   thermostat.down();
   $('#temperature').text(thermostat.getTemperature());
+});
+
+$("#temperature-reset").click(function() { 
+  thermostat.reset();
+  $('#temperature').text(thermostat.getTemperature());
+});
+
+$("#powersaving-off").click(function() { 
+  thermostat.switchOff();
+  $('#temperature').text(thermostat.getTemperature());
+  $('#power-saving-status').text('off');
+});
+
+$("#powersaving-on").click(function() { 
+  thermostat.switchOn();
+  $('#temperature').text(thermostat.getTemperature());
+  $('#power-saving-status').text('on');
 });
 
 });
